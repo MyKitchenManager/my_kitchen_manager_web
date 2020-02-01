8b269daf7ff586fc9736c7fa5e342ebf49c2d28b
Menglin Yu <menglinyu@MacBook-Pro.local>
2020-02-01 02:26:46 -0500

    update Heroku API_URL

--- constant.js
+++ constant.js
@@ -1,3 +1,7 @@
+//production URL
 //export const API_URL = 'https://my-kitchen-manager-prod.herokuapp.com';
-export const API_URL = 'http://localhost:8080';
+//Staging URL
+export const API_URL = 'https://my-kitchen-manager-stag.herokuapp.com';
+//local URL
+//export const API_URL = 'http://localhost:8080';
 export const TOKEN_KEY = 'TOKEN_KEY';
