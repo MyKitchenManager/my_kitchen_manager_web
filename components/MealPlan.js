035860ec36441953f5a807697e549ec7e1b7a2f0
Menglin Yu <menglinyu@MacBook-Pro.local>
2020-02-04 02:25:18 -0500

    calendar view rendered

--- components/MealPlan.js
+++ components/MealPlan.js
@@ -2,45 +2,13 @@ import React, {Component} from 'react';
 import {Content, Text, Container, Card, CardItem} from "native-base"
 import {SafeAreaView, TouchableOpacity, ScrollView, Image} from "react-native";
 import meal from "../assets/meal.jpg";
+import CalendarView from "./CalendarView";
 class MealPlan extends Component {
     render() {
         return (
             <SafeAreaView style={{padding: 3, flex: 1}}>
                 <ScrollView style = {{padding: 30}}>
-                    <Card>
-                        <CardItem header bordered>
-                            <Text> Monday </Text>
-                        </CardItem>
-                        <CardItem >
-                            <Image source={meal} style={{flex:1, height: 200, width:200}}/>
-                        </CardItem>
-                        <CardItem bordered>
-                            <Text>this is the meal</Text>
-                        </CardItem>
-                    </Card>
-                    <Card>
-                        <CardItem header bordered>
-                            <Text> Tuesday </Text>
-                        </CardItem>
-                        <CardItem >
-                            <Image source={meal} style={{flex:1, height: 200, width:200}}/>
-                        </CardItem>
-                        <CardItem bordered>
-                            <Text>this is the meal</Text>
-                        </CardItem>
-                    </Card>
-                    <Card>
-                        <CardItem header bordered>
-                            <Text> Wednesday </Text>
-                        </CardItem>
-                        <CardItem >
-                            <Image source={meal} style={{flex:1, height: 200, width:200}}/>
-                        </CardItem>
-                        <CardItem bordered>
-                            <Text>this is the meal</Text>
-                        </CardItem>
-                    </Card>
-                   {/*<Text>This is meal plan</Text>*/}
+                    <CalendarView />
                 </ScrollView>
             </SafeAreaView>
         );
@@ -48,3 +16,39 @@ class MealPlan extends Component {
 }
 
 export default MealPlan;
+
+
+{/*<Card>*/}
+{/*    <CardItem header bordered>*/}
+{/*        <Text> Monday </Text>*/}
+{/*    </CardItem>*/}
+{/*    <CardItem >*/}
+{/*        <Image source={meal} style={{flex:1, height: 200, width:200}}/>*/}
+{/*    </CardItem>*/}
+{/*    <CardItem bordered>*/}
+{/*        <Text>this is the meal</Text>*/}
+{/*    </CardItem>*/}
+{/*</Card>*/}
+{/*<Card>*/}
+{/*    <CardItem header bordered>*/}
+{/*        <Text> Tuesday </Text>*/}
+{/*    </CardItem>*/}
+{/*    <CardItem >*/}
+{/*        <Image source={meal} style={{flex:1, height: 200, width:200}}/>*/}
+{/*    </CardItem>*/}
+{/*    <CardItem bordered>*/}
+{/*        <Text>this is the meal</Text>*/}
+{/*    </CardItem>*/}
+{/*</Card>*/}
+{/*<Card>*/}
+{/*    <CardItem header bordered>*/}
+{/*        <Text> Wednesday </Text>*/}
+{/*    </CardItem>*/}
+{/*    <CardItem >*/}
+{/*        <Image source={meal} style={{flex:1, height: 200, width:200}}/>*/}
+{/*    </CardItem>*/}
+{/*    <CardItem bordered>*/}
+{/*        <Text>this is the meal</Text>*/}
+{/*    </CardItem>*/}
+{/*</Card>*/}
+{/*<Text>This is meal plan</Text>*/}
