d4978ef1988d89ba6a1404f2bccbdc1b6355c5b9
Menglin Yu <menglinyu@MacBook-Pro.local>
2020-02-14 00:58:02 -0500

    adjust image margin

--- components/Profile.js
+++ components/Profile.js
@@ -178,7 +178,8 @@ const styles = StyleSheet.create({
         width: 200,
         height: 200,
         borderRadius: 100,
-        overflow: "hidden"
+        overflow: "hidden",
+        marginTop: 8
     },
     infoContainer: {
         alignSelf: "center",
