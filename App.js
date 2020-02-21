3e88e1bbd123652309a5910d769fcda8eb8848cc
Menglin Yu <menglinyu@MacBook-Pro.local>
2020-02-20 20:28:29 -0500

    Change UI

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ import {AsyncStorage} from "react-native"
 import {TOKEN_KEY, API_URL} from "./constant";
 import MealPlanStack from './routes/MealPlanStack';
 import MealPlanRecipe from "./components/MealPlanRecipe";
-import Welcome from "./components/Welcome";
 
 export default class App extends Component {
     state = {
@@ -60,14 +59,12 @@ export default class App extends Component {
   render(){
       return  <Router>
                 <Scene key="root">
-                    {/*<Scene key="welcome"  component={Welcome} hideNavBar={true} title="welcome" />*/}
                     <Scene key="login"  component={Login} hideNavBar={true} title="Login" initial={this.state.init}/>
                     <Scene key="signup" component={SignUp} hideNavBar={true} title="SignUp"/>
                     <Scene key="home" data={this.state.userId} component={Home} hideNavBar={true} title = "My Kitchen Manager" initial={!this.state.init}/>
                     <Scene key="recipe" component={MealPlanRecipe} hideNavBar={true} title="MealPlanRecipe" />
                 </Scene>
               </Router>
-
     }
 }
 
